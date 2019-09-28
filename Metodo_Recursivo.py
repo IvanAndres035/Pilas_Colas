56d6156918f96d70c1170a35842e73163c35801f
IVAN ANDRES <andrewimpire32@gmail.com>
2019-09-27 20:29:58 -0500

    Se agregaron los comentarios sobre el proceso para la solucion del programa.

--- Metodo_Recursivo.py
+++ Metodo_Recursivo.py
@@ -4,35 +4,26 @@ class Metodo_Recursivo:
     __b = int(0)
     __Producto = int(1)
 
+#METODO CONSTRUCTOR CON EL ARREGLO Y EL NUMERO DE B
     def __init__(self, ArregloV ,b):
         self.__ArregloV = ArregloV
         self.__b = b
 
+#METODO MULTIPLICAR, EN EL QUE SE REALIZA LA OPERACION DE FORMA ITERATIVA  LA SOLUCION DEL PROGRAMA
     def Multiplicar(self):
+#CICLO FOR; FOR I EN EL RANGO DE  0 HASTA LA LECTURA DEL ARREGLOV.
         for i in range(0, len(self.__ArregloV)):
-            if self.__ArregloV[i] > self.__b:
+
+#SI LOS ELEMENTOS DEL ARREGLOV, SON MAYORES A "B"
+          if self.__ArregloV[i] > self.__b:
+
+#EL PRODUCTO ES IGUAL AL EL PRODUCTO PO LOS ELEMENTOS DEL ARREGLOV
                 self.__Producto = self.__Producto * self.__ArregloV[i]
 
+#METODO PARA OBTENER EL PRODUCTO DE LA OPERACION REALIZADA ANTERIOMENTE
     def getProductos(self):
         return self.__Producto
 
-
-elemento =  Metodo_Recursivo([1,2,3,4,5,6,7,8],4)
+elemento =  Metodo_Recursivo([10,2,11,3,9,2,-1,4],4)
 elemento.Multiplicar()
 print(elemento.getProductos())
-
-
-#Practicas
-""" 
-self.__numeros = input("Numeros del arreglo son: ")
-self.__ArregloV.append(self.__numeros)
-self.__b = int(input("Numero constante: "))
-"""
-
-#ahorita
-'''
-if (self._b <= 1):
-                return self._b
-            else:
-                return self._b * (self._b - 1)
-'''
